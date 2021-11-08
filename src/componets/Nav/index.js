ee476718bef1f5d75ab460ba9c9289c27cfef1e2
Moradi1412 <moradi.javid@gmail.com>
2021-11-08 15:35:21 -0800

    compare about and nav omponenet

--- src/componets/Nav/index.js
+++ src/componets/Nav/index.js
@@ -1,56 +1,55 @@
 import React from 'react';
-
-const categories = [
-    {
-        name: "commercial",
-        description:
-            "Photos of grocery stores, food trucks, and other commercial projects",
-    },
-    { name: "portraits", description: "Portraits of people in my life" },
-    { name: "food", description: "Delicious delicacies" },
-    {
-        name: "landscape",
-        description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-    },
-];
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav() {
 
-    return (
-        <header>
-            <h2>
-                <a data-testid="link" href="/">
-                    <span role="img" aria-label="camera"> 📸</span> Oh Snap!
-                </a>
-            </h2>
-            <nav>
-                <ul className="flex-row">
-                    <li className="mx-2">
-                        <a data-testid="about" href="#about">
-                            About me
-                        </a>
-                    </li>
-                    <li>
-                        <span>Contact</span>
-                    </li>
-                    {categories.map((category) => (
-                        <li
-                            className="mx-1"
-                            key={category.name}
-                        >
-                            <span onClick={() => categorySelected(category.name)} >
-                                {category.name}
-                            </span>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-        </header>
-    );
-}
+    const  categories = [
+      { name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects' },
+      { name: 'portraits', description: 'Portraits of people in my life' },
+      { name: 'food', description: 'Delicious delicacies' },
+      { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' }
+    ];
+    
 
-function categorySelected(name) {
-    console.log(`${name} clicked`)
-};
+    function categorySelected(name) {
+        console.log(`${name} clicked`)
+    };
+
+
+    return (
+      <header className="flex-row px-1">
+        <h2>
+          <a data-testid="link" href="/">
+            <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+          </a>
+        </h2>
+        <nav>
+          <ul className="flex-row">
+            <li className="mx-2">
+              <a data-testid="about" href="#about" onClick={() => categorySelected("About")}>
+                About me
+              </a>
+            </li>
+            <li className={"mx-2"}>
+              <span onClick={() => categorySelected('Contact')}>
+                Contact
+              </span>
+            </li>
+            {
+              categories.map((category) => (
+                <li className="mx-1" key={category.name} >
+                  <span onClick={() => { categorySelected(category.name); }}>
+                   {capitalizeFirstLetter(category.name)}
+                  </span>
+                </li>
+              ))
+            }
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+
+
 
 export default Nav;
