d70b0a7b8ee10de4e894a99d04c14da35e64dc03
Moradi1412 <moradi.javid@gmail.com>
2021-11-13 17:10:54 -0800

    Updates

---
+++ precache-manifest.b16e01c69a0d6f2bd03787a74861795b.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b860dc757a61d84588e26cf0a71f946",
+    "url": "/Rogers-photo-port/index.html"
+  },
+  {
+    "revision": "59d040e839b768e30ad2",
+    "url": "/Rogers-photo-port/static/css/main.5d195ea4.chunk.css"
+  },
+  {
+    "revision": "6078a1a89dc463e0e918",
+    "url": "/Rogers-photo-port/static/js/2.e3890e8f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Rogers-photo-port/static/js/2.e3890e8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59d040e839b768e30ad2",
+    "url": "/Rogers-photo-port/static/js/main.fd1b715b.chunk.js"
+  },
+  {
+    "revision": "a2d1d1f49d0bc5ac562d",
+    "url": "/Rogers-photo-port/static/js/runtime-main.b89b2b08.js"
+  },
+  {
+    "revision": "3cb61cf293675583d9dfd89816632192",
+    "url": "/Rogers-photo-port/static/media/0.3cb61cf2.jpg"
+  },
+  {
+    "revision": "736e95038f8fdb06e7d3ab206ef5c334",
+    "url": "/Rogers-photo-port/static/media/0.736e9503.jpg"
+  },
+  {
+    "revision": "a42cd359d05a17bb84ef14c2aa78c858",
+    "url": "/Rogers-photo-port/static/media/0.a42cd359.jpg"
+  },
+  {
+    "revision": "c0bc02a027cbe8ab45065affd6d1b06e",
+    "url": "/Rogers-photo-port/static/media/0.c0bc02a0.jpg"
+  },
+  {
+    "revision": "100fef3c888ab24aba96228c39e2ea30",
+    "url": "/Rogers-photo-port/static/media/1.100fef3c.jpg"
+  },
+  {
+    "revision": "181f9d5cd7eace3bf69162b96d625d18",
+    "url": "/Rogers-photo-port/static/media/1.181f9d5c.jpg"
+  },
+  {
+    "revision": "6dc96543f430be20924a2dacf5c9007a",
+    "url": "/Rogers-photo-port/static/media/1.6dc96543.jpg"
+  },
+  {
+    "revision": "e05d88b53df4563205cd981e79f5159a",
+    "url": "/Rogers-photo-port/static/media/1.e05d88b5.jpg"
+  },
+  {
+    "revision": "082ff7d9337e64f0ca177035cd46dcca",
+    "url": "/Rogers-photo-port/static/media/2.082ff7d9.jpg"
+  },
+  {
+    "revision": "0c7dce01194c28a9610b0c5efb7b5147",
+    "url": "/Rogers-photo-port/static/media/2.0c7dce01.jpg"
+  },
+  {
+    "revision": "75df3018282379c8eb4984447fa32762",
+    "url": "/Rogers-photo-port/static/media/2.75df3018.jpg"
+  },
+  {
+    "revision": "efb308ab53a3c7639a0fb14a5906ba50",
+    "url": "/Rogers-photo-port/static/media/2.efb308ab.jpg"
+  },
+  {
+    "revision": "0a075f4dbdb1fc069961ccd6ec549b5b",
+    "url": "/Rogers-photo-port/static/media/3.0a075f4d.jpg"
+  },
+  {
+    "revision": "b2912d992e81f84ac3534c54f411ee94",
+    "url": "/Rogers-photo-port/static/media/3.b2912d99.jpg"
+  },
+  {
+    "revision": "c7510d3a5d3bc9d56ae4d0a7676f0a19",
+    "url": "/Rogers-photo-port/static/media/3.c7510d3a.jpg"
+  },
+  {
+    "revision": "ff79cea1d3c7785b55cd7b5265610ad8",
+    "url": "/Rogers-photo-port/static/media/3.ff79cea1.jpg"
+  },
+  {
+    "revision": "74c830fd139bf998cf6991133ce3a834",
+    "url": "/Rogers-photo-port/static/media/4.74c830fd.jpg"
+  },
+  {
+    "revision": "8c4c93ec31fe8ea01cafeda13b675718",
+    "url": "/Rogers-photo-port/static/media/4.8c4c93ec.jpg"
+  },
+  {
+    "revision": "9030776ba38ccfc67b95b903534fa810",
+    "url": "/Rogers-photo-port/static/media/4.9030776b.jpg"
+  },
+  {
+    "revision": "6a2b7a160a2148af430628e2028b3cc5",
+    "url": "/Rogers-photo-port/static/media/cover-image.6a2b7a16.jpg"
+  }
+]);
